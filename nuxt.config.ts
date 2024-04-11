8b5bb080a3bdb30bb3f2190aa11c52295c7d9c4b
ZephirSD <siegfrieddallery@gmail.com>
2024-04-11 21:22:57 +0400

    Configuration de `tailwind`, de google_fonts` en CDN et de SCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  app: {
+    head: {
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
+        },
+      ],
+    },
+  },
+  css: ["~/assets/scss/style.scss"],
+});
